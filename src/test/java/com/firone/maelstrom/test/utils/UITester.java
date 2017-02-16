50cbbc26d101a919c24ae35f121c72ba21ee2628
Romain Roces <romain.roces+github@gmail.com>
2017-02-16 12:14:09 -0500

    Improve tests speed

--- src/test/java/com/firone/maelstrom/test/utils/UITester.java
+++ src/test/java/com/firone/maelstrom/test/utils/UITester.java
@@ -2,23 +2,23 @@ package com.firone.maelstrom.test.utils;
 
 import com.vtence.mario.BrowserDriver;
 import com.vtence.mario.UnsynchronizedProber;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class UITester {
 
-  private BrowserDriver browser;
+  private static BrowserDriver browser;
 
-  @Before
-  public void setUp() throws Exception {
+  @BeforeClass
+  public static void setUp() throws Exception {
     browser = new BrowserDriver(new UnsynchronizedProber(2000, 50), new FirefoxDriver());
   }
 
-  @After
-  public void tearDown() throws Exception {
-    browser().quit();
+  @AfterClass
+  public static void tearDown() throws Exception {
+    browser.quit();
   }
 
   protected BrowserDriver browser() {
