16620aff06146b8eee17f263689f7cb57cba1c84
Romain Roces <romain.roces+github@gmail.com>
2017-02-24 13:34:20 -0500

    Add tests about search button in pages of "Network list" and "Studies list"

---
+++ src/test/java/com/firone/maelstrom/test/drupal/study/StudySearchNavigation.java
@@ -0,0 +1,19 @@
+package com.firone.maelstrom.test.drupal.study;
+
+import com.firone.maelstrom.test.utils.By;
+import com.firone.maelstrom.test.utils.UITester;
+import org.junit.Test;
+
+public class StudySearchNavigation extends UITester {
+
+  @Test
+  public void search_button_on_network_list_must_return_search_result_for_all_networks() {
+
+    browser().navigate().to("http://localhost/drupal/mica/studies");
+
+    browser().element(By.text("Search Studies")).click();
+
+    browser().element(By.ref("search-criterion")).hasText("Acronym:any");
+    browser().element(By.ref("search-results", "acronym", "[1]")).hasText("Atlantic PATH");
+  }
+}
